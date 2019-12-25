1bc83de6ba4e9850a0fe20e3f91116564a04d0b1
ntolley <55253912+ntolley@users.noreply.github.com>
2019-12-25 13:39:02 -0800

    Succesful instantiation, needed to copy blackrock .dll and .lib files to plugin-GUI debug directory

--- NSPSource_test/Plugin/Source/Blackrock.h
+++ NSPSource_test/Plugin/Source/Blackrock.h
@@ -10,16 +10,16 @@ public:
 	CBlackrock(void);
 	virtual ~CBlackrock(void);
 
-	//int connection_init_EMG(void);
-	//void connection_end(void);
-//	void get_EMG(const int, const int, const int* const, INT16*, INT16*, INT16**);
-//	void CBlackrock::get_data(std::vector<int>, std::vector<int>, INT16** data);
-//	void get_spikes(UINT32***, double***, double*);
-//	UINT32 getTime(void);
-//	void setComment(UINT32, std::string);
+	int connection_init_EMG(void);
+	void connection_end(void);
+	void get_EMG(const int, const int, const int* const, INT16*, INT16*, INT16**);
+	void CBlackrock::get_data(std::vector<int>, std::vector<int>, INT16** data);
+	void get_spikes(UINT32***, double***, double*);
+	UINT32 getTime(void);
+	void setComment(UINT32, std::string);
 	void setup_connection_blackrock(void);
-//	int connection_init_neural(void);
-//
+	int connection_init_neural(void);
+
 private:
 	cbSdkTrialEvent m_trialEvent;
 	cbSdkTrialCont m_trialCont;
